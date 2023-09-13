e9a94b89b0be7c093f81d9f0d602e6a056e221ae
ND <nicolas.derambure@septeo.com>
2023-09-13 15:54:31 +0200

    Debut des component wrap

--- src/components/Animal/Animal.tsx
+++ src/components/Animal/Animal.tsx
@@ -1,24 +1,26 @@
 import "./Animal.css";
 import AnimalDetails from "../AnimalDetails/AnimailDetails";
 import { IAnimal, IAnimalAdditional } from "./IAnimal";
-import { IAnimalDetails } from "../AnimalDetails/IAnimalDetails";
+import Card from "../Card/Card";
 
-function showAdditional(additional? : IAnimalAdditional) {
-  const alertInfo = Object.entries(additional)
-    .map((informations) => `${informations[0]}: ${informations[1]}`)
-    .join("\n");
-  alert(alertInfo);
+function showAdditional(additional?: IAnimalAdditional) {
+  if (additional != undefined) {
+    const alertInfo = Object.entries(additional)
+      .map((informations) => `${informations[0]}: ${informations[1]}`)
+      .join("\n");
+    alert(alertInfo);
+  }
 }
 
 export default function Animal(animal: IAnimal) {
   return (
-    <div className="animaux ">
+      <Card title="Animal">
       <h2>{animal.name}</h2>
       <h4>{animal.size}</h4>
       <button onClick={() => showAdditional(animal.additional)}>
         <span role="img">{animal.emoji}</span>
       </button>
-      <AnimalDetails {...animal}/>
-    </div>
+      <AnimalDetails { ...animal } />
+      </Card>
   );
 }
