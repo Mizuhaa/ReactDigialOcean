0730ea7080565fc60bece4f4873b40e105ac811e
Mizuhaa <nico.deram34470@gmail.com>
2023-09-14 16:53:10 +0200

    Avait utilisation du context

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
       <Instructions />
       <div className="flex flex-row justify-between px-5 text-center divide-x-2">
         {animals.map((animal) => (
-          <div className="grid grid-rows-2"><Animal {...animal} /><AnimalStockHandler {...animal} /></div>
+          <div className="grid grid-rows-2"><Animal {...animal} /><AnimalStockHandler {...animal}/></div>
         ))}
       </div>
     </div>
--- src/components/AnimalStockHandler/AnimalStockHandler.tsx
+++ src/components/AnimalStockHandler/AnimalStockHandler.tsx
@@ -8,6 +8,7 @@ export default function AnimalStockHandler(animal: IAnimal) {
     return (
       <div>
         <p>Total animals in the zoo : {cart.length}</p>
+        <p>Totaling {cart.reduce((sum, current) => sum + current.size, 0)}kg</p>
         <p>Currently {cart.length} in the zoo</p>
         <div className="space-x-5">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {setCart([...cart, animal])}}>
