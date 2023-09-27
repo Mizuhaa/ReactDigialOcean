cba9cb08805aaa32574392ded7e4dbca41e63b1a
Alexis Ribot <118186663+alexisribot@users.noreply.github.com>
2023-09-27 22:40:34 +0200

    fix(form) :bug: fix form reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,79 @@
-import animalsDataSet from "./components/App/data";
-import { Animal as IAnimal } from "./models";
-import Animal from "./components/Animal/Animal";
+import { useEffect, useState } from "react";
 import "./App.css";
-import AnimalStockHandler from "./components/AnimalStockHandler/AnimalStockHandler";
-import { useState, useEffect } from "react";
 import { ZooContext } from "./Context/ZooContext";
+import Animal from "./components/Animal/Animal";
+import AnimalStockHandler from "./components/AnimalStockHandler/AnimalStockHandler";
+import animalsDataSet from "./components/App/data";
 import ZooNamePicker from "./components/ZooNamePicker/ZooNamePicker";
-import ZooVisitorForm from "./components/ZooVisitorForm/ZooVisitorForm";
+import { ZooVisitorForm } from "./components/ZooVisitorForm/ZooVisitorForm";
+import { Animal as IAnimal } from "./models";
 
 export default function App() {
-  const [animals, setAnimals] = useState<IAnimal[]>([]);
-  const [name, setName] = useState<string>("");
-  const [alert, setAlert] = useState(false);
+    const [animals, setAnimals] = useState<IAnimal[]>([]);
+    const [name, setName] = useState<string>("");
+    const [alert, setAlert] = useState(false);
 
+    useEffect(() => {
+        const handleWindowClick = () => setAlert(false);
+        if (alert) {
+            window.addEventListener("click", handleWindowClick);
+        } else {
+            window.removeEventListener("click", handleWindowClick);
+        }
+        return () => window.removeEventListener("click", handleWindowClick);
+    }, [alert, setAlert]);
 
-  useEffect(() => {
-    const handleWindowClick = () => setAlert(false)
-    if (alert) {
-      window.addEventListener('click', handleWindowClick);
-    } else {
-      window.removeEventListener('click', handleWindowClick)
-    }
-    return () => window.removeEventListener('click', handleWindowClick);
-  }, [alert, setAlert]);
-
-  return (
-    <ZooContext.Provider
-      value={{
-        animals,
-        setAnimals,
-        name,
-        setName
-      }}
-    >
-      <div className="dark:bg-gray-900 dark:text-gray-50">
-        <ZooNamePicker />
-        {/* <Welcome />
+    return (
+        <ZooContext.Provider
+            value={{
+                animals,
+                setAnimals,
+                name,
+                setName,
+            }}
+        >
+            <div className="dark:bg-gray-900 dark:text-gray-50">
+                <ZooNamePicker />
+                {/* <Welcome />
         <Instructions /> */}
-        <div>
-          <div>
-            <h1 className="underline text-5xl">
-              Liste des animaux du zoo {name} :
-            </h1>
-            <div className="p-5 text-2xl">
-              {animals.map((animal) => animal.name).join(", ")}
-              {<p>Total animals in the zoo : {animals.length}</p>}
-            </div>
-            {/* <div className="">{animals.map((animal) => animal.name)}</div> */}
-          </div>
-        </div>
-        <h1 className="capitalize underline text-5xl p-5">Gestion du zoo</h1>
-        <div className="flex flex-row justify-between px-5 text-center divide-x-2">
-          {animalsDataSet.map((animal) => (
-            <div className={`bg-gradient-to-t from-black/50
+                <div>
+                    <div>
+                        <h1 className="underline text-5xl">Liste des animaux du zoo {name} :</h1>
+                        <div className="p-5 text-2xl">
+                            {animals.map((animal) => animal.name).join(", ")}
+                            {<p>Total animals in the zoo : {animals.length}</p>}
+                        </div>
+                        {/* <div className="">{animals.map((animal) => animal.name)}</div> */}
+                    </div>
+                </div>
+                <h1 className="capitalize underline text-5xl p-5">Gestion du zoo</h1>
+                <div className="flex flex-row justify-between px-5 text-center divide-x-2">
+                    {animalsDataSet.map((animal) => (
+                        <div
+                            className={`bg-gradient-to-t from-black/50
             w-auto h-auto
             border-2 border-red-400
-            `}>
-              <div className="">
-                <Animal animal={animal} />
-                <AnimalStockHandler animal={animal}>
-                  <button
-                    className="bg-emerald-300 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={() => {
-                      setAnimals([...animals, animal]);
-                    }}
-                  >
-                    Add{" "}
-                  </button>
-                </AnimalStockHandler>
-              </div>
-              <img src={animal.picture} className="-z-10" />
+            `}
+                        >
+                            <div className="">
+                                <Animal animal={animal} />
+                                <AnimalStockHandler animal={animal}>
+                                    <button
+                                        className="bg-emerald-300 hover:bg-emerald-700 text-white font-bold py-2 px-4 rounded"
+                                        onClick={() => {
+                                            setAnimals([...animals, animal]);
+                                        }}
+                                    >
+                                        Add{" "}
+                                    </button>
+                                </AnimalStockHandler>
+                            </div>
+                            <img src={animal.picture} className="-z-10" />
+                        </div>
+                    ))}
+                </div>
+                <ZooVisitorForm />
             </div>
-          ))}
-        </div>
-        <ZooVisitorForm />
-      </div>
-    </ZooContext.Provider>
-  );
+        </ZooContext.Provider>
+    );
 }
--- src/components/ZooVisitorForm/ZooVisitorForm.tsx
+++ src/components/ZooVisitorForm/ZooVisitorForm.tsx
@@ -1,96 +1,127 @@
-
-import React, { useState, useReducer, ChangeEvent, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useReducer, useState } from "react";
 
 interface FormData {
-  [key: string]: string;
+    avis: string;
+    count: number;
+    paysSelector: string;
+    checkBoxLike: boolean;
 }
 
-const formReducer = (state: FormData, event: { name: string; value: string }) => {
-  if (event.reset) {
-    return {
-      Avis: '',
-      count: 10,
-      paysSelector: '',
-      'checkBoxLike': false,
+const initialFormData: FormData = {
+    avis: "",
+    count: 10,
+    paysSelector: "",
+    checkBoxLike: false,
+};
+
+type FormAction = { name: string; value: string | number } | { reset: true };
+
+const formReducer = (state: FormData, action: FormAction) => {
+    if ("reset" in action && action.reset) {
+        return initialFormData;
     }
-  }
-  return {
-    ...state,
-    [event.name]: event.value
-  }
-}
+    if ("name" in action) {
+        return {
+            ...state,
+            [action.name]: action.value,
+        };
+    }
+    return state;
+};
 
 export function ZooVisitorForm() {
-  const [formData, setFormData] = useReducer(formReducer, {count: 10});
-  const [submitting, setSubmitting] = useState(false);
+    const [formData, setFormData] = useReducer(formReducer, initialFormData);
+    const [submitting, setSubmitting] = useState(false);
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    setSubmitting(true);
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        setSubmitting(true);
 
-    setTimeout(() => {
-      setSubmitting(false);
-      setFormData({
-        reset: true
-      })
-    }, 3000)
-  };
+        setTimeout(() => {
+            setSubmitting(false);
+            setFormData({
+                reset: true,
+            });
+        }, 3000);
+    };
 
+    const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const { name, value, type } = event.target;
+        const isCheckbox = type === "checkbox";
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
-    const isCheckbox = event.target.type === 'checkbox';
-    setFormData({
-      name: event.target.name,
-      value: isCheckbox ? (event.target as HTMLInputElement).checked.toString() : event.target.value,
-    });
-  }
+        setFormData({
+            name,
+            value: isCheckbox ? (event.target as HTMLInputElement).checked.toString() : value,
+        });
+    };
 
-  return (
-    <div className="p-5">
-      <h1>Qu'avez vous pensé du zoo?</h1>
-      {submitting &&
-        <div>Submtting Form...
-          <ul>
-            {Object.entries(formData).map(([name, value]) => (
-              <li key={name}><strong>{name}</strong>:{value.toString()}</li>
-            ))}
-          </ul>
+    return (
+        <div className="p-5">
+            <h1>Qu'avez vous pensé du zoo?</h1>
+            {submitting && (
+                <div>
+                    Submitting Form...
+                    <ul>
+                        {Object.entries(formData).map(([name, value]) => (
+                            <li key={name}>
+                                <strong>{name}</strong>: {value.toString()}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <form onSubmit={handleSubmit} className="flex flex-col space-y-4 w-40">
+                <fieldset>
+                    <label>
+                        <p>Avis</p>
+                        <input
+                            name="avis"
+                            type="text"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-emerald-800 dark:border-emerald-600 dark:placeholder-emerald-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            onChange={handleChange}
+                            value={formData.avis}
+                        />
+                    </label>
+                </fieldset>
+                <fieldset>
+                    <label>
+                        <p>De quel pays venez-vous ?</p>
+                        <select
+                            name="paysSelector"
+                            onChange={handleChange}
+                            value={formData.paysSelector}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"
+                        >
+                            <option value="">--Choisissez une option--</option>
+                            <option value="france">France</option>
+                            <option value="spain">Espagne</option>
+                            <option value="italy">Italie</option>
+                        </select>
+                    </label>
+                    <label>
+                        <p>Combien étiez-vous?</p>
+                        <input
+                            type="number"
+                            name="count"
+                            onChange={handleChange}
+                            value={formData.count}
+                            step="1"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-emerald-800 dark:border-emerald-600 dark:placeholder-emerald-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                    </label>
+                    <label className="flex flex-row py-3 space-x-3 cursor-pointer">
+                        <p>Envoyez un like</p>
+                        <input type="checkbox" name="checkBoxLike" onChange={handleChange} checked={formData.checkBoxLike} />
+                    </label>
+                </fieldset>
+                <button
+                    type="submit"
+                    className="p-2 rounded-md bg-slate-500 text-2xl text-white dark:text-emerald-50 cursor-pointer border-emerald-300"
+                    disabled={submitting || !formData.avis || !formData.paysSelector || !formData.count}
+                >
+                    Valider
+                </button>
+            </form>
         </div>
-
-      }
-      <form onSubmit={handleSubmit}>
-        <fieldset className="my-5">
-          <label>
-            <p>Avis</p>
-            <input name="Avis"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-emerald-800 dark:border-emerald-600 dark:placeholder-emerald-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              onChange={handleChange}
-              value={formData.Avis || ''} />
-          </label>
-        </fieldset>
-        <fieldset>
-          <label>
-            <p>De quel pays venez-vous ?</p>
-            <select name="paysSelector" onChange={handleChange} value={formData.paysSelector || ''} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500'>
-              <option value="">--Choisissez une option--</option>
-              <option value="france">France</option>
-              <option value="spain">Espagne</option>
-              <option value="italy">Italie</option>
-            </select>
-          </label>
-          <label>
-            <p>Combien étiez-vous?</p>
-            <input type="number" name="count" onChange={handleChange} value={formData.count || ''} step="1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-emerald-800 dark:border-emerald-600 dark:placeholder-emerald-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-          </label>
-          <label className="flex flex-row py-3 space-x-3">
-            <p>Envoyez un like</p>
-            <input type="checkbox" name="checkBoxLike" onChange={handleChange} checked={formData.checkBoxLike || false} />
-          </label>
-        </fieldset>
-        <button type="submit" className="text-2xl text-emerald-800 dark:text-emerald-50 cursor-pointer border border-emerald-300 hover:border-red-800">Valider</button>
-      </form>
-    </div>
-  )
+    );
 }
-
-export default ZooVisitorForm;
